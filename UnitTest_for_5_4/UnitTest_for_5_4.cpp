2cab18710275b4d465e7ec2e2f8a4b299b925a25
MarichkaPM <tanechnyk.marichka@gmail.com>
2022-11-03 23:50:58 +0200

    first commit

---
+++ UnitTest_for_5_4/UnitTest_for_5_4.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Laboratorna 5_4/Lab 5_4.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestfor54
+{
+	TEST_CLASS(UnitTestfor54)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double a;
+			a = S0(10, 20);
+			Assert::AreEqual(a, 0.644021, 0.0001);
+		}
+	};
+}
